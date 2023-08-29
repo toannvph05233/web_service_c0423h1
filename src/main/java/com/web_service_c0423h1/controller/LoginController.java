1ef17e782759faa45f3a9c54369b71d6265bb459
johntoan98 <toan.nguyen@codegym.vn>
2023-08-29 15:55:09 +0700

    up jwt

--- src/main/java/com/web_service_c0423h1/controller/LoginController.java
+++ src/main/java/com/web_service_c0423h1/controller/LoginController.java
@@ -1,6 +1,8 @@
 package com.web_service_c0423h1.controller;
 
 import com.web_service_c0423h1.model.Account;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -12,6 +14,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Date;
+
 @RestController
 @CrossOrigin("*")
 public class LoginController {
@@ -19,10 +23,25 @@ public class LoginController {
     AuthenticationManager authenticationManager;
 
     @PostMapping("login")
-    public UserDetails login(@RequestBody Account account) {
+    public String login(@RequestBody Account account) {
         Authentication authentication = authenticationManager.authenticate(
                 new UsernamePasswordAuthenticationToken(account.getUsername(), account.getPassword()));
         SecurityContextHolder.getContext().setAuthentication(authentication);
-        return (UserDetails) authentication.getPrincipal();
+
+        String token = createToken(account.getUsername());
+        return token;
+    }
+
+
+    public static final String PRIVATE_KEY = "123456789999887abc";
+    private static final long EXPIRE_TIME = 86400000000L;
+    // hàm tạo ra token
+    public String createToken(String username) {
+        return Jwts.builder()
+                .setSubject((username))
+                .setIssuedAt(new Date())
+                .setExpiration(new Date((new Date()).getTime() + EXPIRE_TIME * 1000))
+                .signWith(SignatureAlgorithm.HS512, PRIVATE_KEY)
+                .compact();
     }
 }
